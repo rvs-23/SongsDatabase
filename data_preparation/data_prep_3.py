8b2f30521ffb6a141e94f1edfaf22d73f7144515
23rishavsharma@gmail.com <23rishavsharma@gmail.com>
2022-01-09 02:01:26 +0530

    commit day 4. Figured out how to fetch video details

--- data_preparation/data_prep_3.py
+++ data_preparation/data_prep_3.py
@@ -1,53 +1,51 @@
 r'''
 
-This script does the following:
-    - For a given watched YouTube video link, extracts the video id.
-    - For a given video id, it fetches all the relevant information which include:
-        - description
-        - 
-    - Creates a dictionary of information and adds it to the file.
-
 '''
+from googleapiclient.discovery import build
+from pprint import pprint
+import os
+import pandas as pd
 
+base_path = os.path.dirname(__file__)
+api_file = os.path.join(base_path, '../_private_data/api_key.txt')
 
+with open(api_file) as file:
+    api_key = file.read()
 
+def get_video_details(youtube_build, video_ids):
+    '''
+    Function to get the 
+    video_ids -> list of video ids whose details we want to fetch
+    
+    '''
+    # YouTube limits to only 50 requests per execute so we need to divide
+    # the video_ids into batches of 50 to fetch the details.
+    
+    all_video_details = list()
+    
+    for limit in range(0, len(video_ids), 50):
+        request = youtube_build.videos().list(part='snippet', id=','.join(video_ids))
+        response = request.execute()
+        # The response object will have the details of all 50 videos.
+        # So, we loop over to extract info one by one. The 'items' key contains
+        # all the details that we are after
+        for video in response['items']:
+            snippet = video['snippet']
+            video_detail = dict(
+                Title=snippet['title'],
+                Category=snippet['categoryId'],
+                PublishDate=snippet['publishedAt'],
+                ChannelTitle=snippet['channelTitle'],
+                Description=snippet['description'],
+                Tags=snippet['tags'],
+                )
+            
+            all_video_details.append(video_detail)
+        
+    return all_video_details
 
+yt = build('youtube', 'v3', developerKey=api_key)
+pprint(get_video_details(yt, ['WnU0lH6C0EA', '5MkfBNl_3pw']))
 
 ####################################################################
 
-# from googleapiclient.discovery import build
-
-
-# api_key = os.environ.get('YT-API_key_file')
-
-# youtube = build('youtube', 'v3', developerKey=api_key)
-
-# req = youtube.channels().list(
-#     part='statistics',
-#     forUsername='schafer5'
-#     )
-
-# response = req.execute()
-
-# print(response)
-
-######################################################################
-
-# import json
-# import urllib
-
-# #change to yours VideoID or change url inparams
-# VideoID = "3tfyYbCzfKk" 
-
-# params = {"format": "json", "url": "https://www.youtube.com/watch?v=%s" % VideoID}
-# url = "https://www.youtube.com/oembed"
-# query_string = urllib.parse.urlencode(params)
-# url = url + "?" + query_string
-# print(query_string)
-# print(url)
-# with urllib.request.urlopen(url) as response:
-#     response_text = response.read()
-#     data = json.loads(response_text.decode())
-#     print(data['title'])
-
-##########################################################################    
